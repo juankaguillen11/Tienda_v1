e0525f2c5ee8811853e537c4dcf8de6b8f31051a
juankaguillen11 <135542623+juankaguillen11@users.noreply.github.com>
2024-11-14 21:56:00 -0600

    Semana9
    Semana 9

--- src/main/java/com/tienda/dao/ProductoDao.java
+++ src/main/java/com/tienda/dao/ProductoDao.java
@@ -5,8 +5,9 @@ package com.tienda.dao;
  * @author juankaguillen
  */
 import com.tienda.domain.Categoria;
+import com.tienda.domain.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoriaDao extends JpaRepository <Categoria,Long> {
+public interface ProductoDao extends JpaRepository <Producto,Long> {
     
 }
--- src/main/java/com/tienda/domain/Categoria.java
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -12,6 +12,7 @@ package com.tienda.domain;
 import jakarta.persistence.*;
 import java.io.Serializable;
 import lombok.Data;
+import java.util.List;
 
 @Data
 @Entity
@@ -28,6 +29,10 @@ public class Categoria implements Serializable {
     private String rutaImagen;
     private boolean activo;
 
+    @OneToMany
+    @JoinColumn(name="id_categoria", updatable = false)
+    List<Producto> productos;
+    
     public Categoria() {
     }
 
